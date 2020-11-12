bd14e7c350b18351d60a8c1140073902ccbab393
AndrasMarro <72513821+AndrasMarro@users.noreply.github.com>
2020-11-12 08:59:18 +0100

    Memory V1

---
+++ vl.js
@@ -0,0 +1,6 @@
+// let card = document.getElementsByClassName("card");
+// let cards = [...card];
+
+// for (var i = 0; i < cards.length; i++){
+//     cards[i].addEventListener("click", displayCard);
+//  };
